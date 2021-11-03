cac060e9421e13aa62fe631d0c8491598a463a23
stagiaire <stagiaire@9FDTJ5J.rondeau.dawan.fr>
2021-11-03 16:25:47 +0100

    Script manage_dir pour creation de repertoire sur variable (s'adapte au contexte)

---
+++ manage_dir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Script :
+#En utilisant des variables
+set -x
+
+#Variabilise le nom du repertoire : rep=backup
+rep=backup
+
+#Création d'un repertoire backup dans la home directory du user qui lance le script (mkdir)
+
+echo "Creation du repertoire $HOME/$rep" 
+mkdir $HOME/$rep 2>/dev/null  # mise en rebut de la sortie stderr
+#mkdir ~/$rep
+
+#Affichage des propriété du répertoire backup (ls -ld rep)
+echo "Propriétés :"
+echo "ls -ld  $HOME/$rep"
--- premier_script.sh
+++ premier_script.sh
@@ -6,9 +6,27 @@
 
 # Ceci est un premier script shell avec interpreteur : bash
 
+# Declaration d'une variable
+mavariable="Lancement sauvegarde..."
+
+var2="script"
+
 # Appel de la commande echo pour afficher quelque chose
 echo Hello World !
 
 #ls -rien
 
 echo "Suite..."
+
+# Melange texte et variable
+echo "Action : $mavariable"
+
+# Concatenation
+echo "${mavariable}${var2}"
+echo "${var2}ing"
+
+# redeclaration d'une variable existante
+var2="FIN"
+
+echo "${var2}"
+
