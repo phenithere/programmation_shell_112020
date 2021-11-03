668f1a4015ab43a220c551252ffee7089e2697c1
stagiaire <stagiaire@9FDTJ5J.rondeau.dawan.fr>
2021-11-03 15:32:30 +0100

    utilisation commande set pour debug

--- premier_script.sh
+++ premier_script.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Utilisation commande set pour debug
+# set -x
+# set -xe 
+
 # Ceci est un premier script shell avec interpreteur : bash
 
 # Appel de la commande echo pour afficher quelque chose
 echo Hello World !
+
+#ls -rien
+
+echo "Suite..."
