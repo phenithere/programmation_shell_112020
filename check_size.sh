330b0725ccb52b497df53497e89ec7895b68e66a
stagiaire <stagiaire@9FDTJ5J.rondeau.dawan.fr>
2021-11-04 12:39:18 +0100

    tp check-size avec boucle for et conditions sur test

---
+++ check_size.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# Script d'analyse et retour de taille de répertoires
+
+# Le script attend un argument en entrée : /rep
+# Usage : ./check_size.sh /rep
+
+# Vérifier que le script est bien lance avec un argument
+# si pas lancé avec un argument et informer et quitter
+#if [ -z "$1" ]
+if [ $# -ne 1 ]
+then
+  echo "Nombre d'arguments passés invalide"
+  echo "Usage: $(basename $0) <repertoire>"
+  exit 1  
+fi
+
+
+# verifier que le repertoire donné en argument existe
+if [ -d $1 ]
+then
+# Boucler avec for sur le repertoire parent afin d'afficher la taille des repertoires presents
+# utilisation de la substitution de commande pour alimenter la boucle for (ls)
+# du -sh {element}
+  for i in $(ls $1)
+  do
+    du -sh $1/$i
+  done
+else
+  echo "Le repertoire $1 n'existe pas"
+  exit 1
+fi
