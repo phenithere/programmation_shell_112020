1f45d85ee0478d482ff1204b8d1e2b18686e7be1
stagiaire <stagiaire@9FDTJ5J.rondeau.dawan.fr>
2021-11-05 10:42:41 +0100

    creation fonctions de tests et reformulation tp

--- archive.sh
+++ archive.sh
@@ -1,9 +1,63 @@
 #!/bin/bash
 
+#set -x
+
 # Script de recherche et archivage de fichiers 
 
-# Le script doit permettre de définir une date de modification de fichier (ex 15) pour rechercher les fichier de plus de 15j par exemple
+# Creation du répertoire d'accueil
+mkdir -p ${HOME}/backup
+
+
+# Fonction aide
+function usage {
+  echo "Usage: $(basename $0) [jours] <repertoire>"
+  echo " "
+  echo "jours: nombre décimal"
+  echo "repertoire: chemin absolu"
+  echo " "
+}
+
+function verif_int {
+  if [ ! -z "${1##*[!0-9]*}" ]
+  then
+    jour=$1
+    return 0   
+  else
+    echo "$1 n'est pas un entier"
+    exit 1
+  fi
+}
+
+function verif_rep {
+  if [[ -d $1 ]]
+  then
+    return 0
+  else
+    echo "Repertoire $1 inexistant"
+    exit 1
+  fi
+}
+
+case $# in
+  1)
+    verif_rep $1 \
+    #&& find $1 -type f 
+    ;;
+  2)
+    verif_int $1 && verif_rep $2
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+esac
+
+
+  
+
+# Le script doit permettre de demande un entier (ex 15) pour rechercher les fichier de plus de 15j par exemple
 # => VARIABLE : read ou arguments (vérifier s'il est bien numérique)
+
 
 # Le script doit permettre de définir un répertoire à analyser
 # => VARIABLE : read ou arguments (vérifier s'il existe)
@@ -13,11 +67,12 @@
 
 # Le script lance une commande find pour rechercher les éléments de type fichier qui ont pour name un pattern "*.log*" de plus de 15j et les copie dans /backup
 # commande find : man find
-
+# find $rep -type f -name ".log" -mtime $jour -exec cp {} "${HOME}/backup" \;
 
 
 # Le script créé une archive dans la home du user avec pour nom : log.{date formatée}.tar.gz et pour contenu les fichier du répertoire /backup
-# tar 
+# utilisation de la substitution de commande pour mettre un date formatée dans le nom de l'archive
+# tar -czf ~/log.$(date +%d%m%Y).tar.gz
 
 
 ## On peut réaliser des fonctions 
