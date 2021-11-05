543ab311e6d7adb316771cb7d2985eec8b24717c
stagiaire <stagiaire@9FDTJ5J.rondeau.dawan.fr>
2021-11-05 09:40:41 +0100

    Ajout tp archive.sh

--- archive.sh
+++ archive.sh
@@ -3,14 +3,21 @@
 # Script de recherche et archivage de fichiers 
 
 # Le script doit permettre de définir une date de modification de fichier (ex 15) pour rechercher les fichier de plus de 15j par exemple
-# => read ou arguments (vérifir s'il est bien numérique)
+# => VARIABLE : read ou arguments (vérifier s'il est bien numérique)
 
 # Le script doit permettre de définir un répertoire à analyser
-# => read ou arguments (vérifier s'il existe)
+# => VARIABLE : read ou arguments (vérifier s'il existe)
 
-# Le script créé un répertoire backup dans la home du user qui lance le script
+# Le script créé un répertoire backup dans la home du user qui lance le script (ou vérifier s'il existe)
+# mkdir
 
 # Le script lance une commande find pour rechercher les éléments de type fichier qui ont pour name un pattern "*.log*" de plus de 15j et les copie dans /backup
+# commande find : man find
 
-# Le script créé une archive dans la home du user avec pour nom : log.{date formatée].tar.gz et pour contenu les fichier du répertoire /backup
 
+
+# Le script créé une archive dans la home du user avec pour nom : log.{date formatée}.tar.gz et pour contenu les fichier du répertoire /backup
+# tar 
+
+
+## On peut réaliser des fonctions 
